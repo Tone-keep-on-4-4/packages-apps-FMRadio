9eddeb76ee0529d1d6c02190824bdb7528dca1a0
sjllls <2012ji123@gmail.com>
2020-03-21 14:14:27 +0800

    For Pie: LOCAL_PRIVATE_PLATFORM_APIS := true

--- Android.mk
+++ Android.mk
@@ -24,6 +24,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := FMRadio
 LOCAL_JNI_SHARED_LIBRARIES := libfmjni
 
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
 LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_PRIVILEGED_MODULE := true
 
